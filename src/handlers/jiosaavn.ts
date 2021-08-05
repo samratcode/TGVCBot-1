72f09eebda77a8bdf1d47dab1934e7e53f67d7ef
S <85238140+samratcode@users.noreply.github.com>
2021-08-05 14:06:53 +0530

    Update jiosaavn.ts

--- src/handlers/jiosaavn.ts
+++ src/handlers/jiosaavn.ts
@@ -5,7 +5,7 @@ import { commandExtractor } from '../utils';
 import { JioSaavnSongResponse, JioSaavnSongSearchResponse } from '../types/jiosaavn.response';
 import { playOrQueueSong } from '../tgcalls';
 
-export const JioSaavnPlay = Composer.command(['jiosaavn', 'jsvn'], async (ctx) => {
+export const JioSaavnPlay = Composer.command(['splay', 'js'], async (ctx) => {
     
     await ctx.telegram.sendChatAction(ctx.chat.id, "typing");
 
